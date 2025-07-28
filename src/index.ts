38bfc23381f023bc1a18a64986dc11e73136892f
Bikash Shaw <vshaw138@gmail.com>
2025-07-28 17:18:47 +0530

    teted hono for http requests

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,28 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.post('/api/v1/signup', async (c) => {
+  const body = await c.req.json()
+  return c.json({
+    message: "User Created"
+  })
+})
+
+app.post('/api/v1/signin', async (c, next) => {
+  if(c.req.header("Authorization")){
+    console.log("Entering Authorized Route")
+    await next()
+  }
+  else{
+    return c.json({
+      message: "User not Authorized"
+    })
+  }
+})
+app.post('/api/v1/createTodo', (c) => {
+  return c.text('Hello Hono!')
+})
+app.get('/api/v1/getTodo', (c) => {
   return c.text('Hello Hono!')
 })
 
